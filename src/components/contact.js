123d28c2206edd3f79d06c7703cd8f3596683c94
Milton Ly <ly.milton@gmail.com>
2023-03-06 01:18:26 -0800

    initial commit, Carousel.js, contact.js added

---
+++ src/components/contact.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BsLinkedin, AiFillGithub, CgMail } from "react-bootstrap-icons"
+
+function Contact(){
+    return (
+        <div>
+            <section id="contact" className="contact-section">
+                <div className="container-1">
+                    <div className="contact-info">
+                        <h2>Contact me at:</h2>
+                    </div>
+                    <ul className="contact-icons">
+                        <li>
+                            <a href="/"><BsLinkedin/></a>
+                        </li>
+                        <li>
+                            <a href="/"><AiFillGithub/></a>
+                        </li>
+                        <li>
+                            <a href="/"><CgMail/></a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Contact
