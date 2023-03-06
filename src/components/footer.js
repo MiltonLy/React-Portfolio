4165032fce6053b044bceabb75f6c89135309ae2
Milton Ly <ly.milton@gmail.com>
2023-03-06 01:40:38 -0800

    footer added

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            <footer className="footer">
+                <div className="container">
+                    <p>Milton Ly 2023</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
